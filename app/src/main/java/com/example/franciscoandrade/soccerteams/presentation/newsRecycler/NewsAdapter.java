4c0023b332848ca3597c0f22f2cf73a19c938ca2
7sco <xaviandrade14@gmail.com>
2018-06-02 00:25:10 -0400

    news viewholder created

---
+++ app/src/main/java/com/example/franciscoandrade/soccerteams/presentation/newsRecycler/NewsAdapter.java
@@ -0,0 +1,23 @@
+package com.example.franciscoandrade.soccerteams.presentation.newsRecycler;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsViewHolder> {
+    @NonNull
+    @Override
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
