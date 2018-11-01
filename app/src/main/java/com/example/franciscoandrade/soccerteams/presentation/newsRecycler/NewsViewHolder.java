9bfa75f91872963f0eb1266e409b75950c3854fe
7sco <xaviandrade14@gmail.com>
2018-11-01 00:26:57 -0400

    last commit

--- app/src/main/java/com/example/franciscoandrade/soccerteams/presentation/newsRecycler/NewsViewHolder.java
+++ app/src/main/java/com/example/franciscoandrade/soccerteams/presentation/newsRecycler/NewsViewHolder.java
@@ -1,9 +1,12 @@
 package com.example.franciscoandrade.soccerteams.presentation.newsRecycler;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,7 +19,7 @@ import com.squareup.picasso.Picasso;
 
 import static com.example.franciscoandrade.soccerteams.R.color.bkg_news2;
 
-public class NewsViewHolder extends RecyclerView.ViewHolder {
+public class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     @BindView(R.id.container)
     ConstraintLayout cont;
     @BindView(R.id.news_tittle)
@@ -26,21 +29,31 @@ public class NewsViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.newsImage)
     ImageView newsImage;
 
+    private News.Articles articles;
+
 
     public NewsViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+        itemView.setOnClickListener(this);
     }
 
     public void bind(News.Articles articles, int position) {
+        this.articles= articles;
         if (position%2 ==0 ){
             cont.setBackgroundColor(itemView.getResources().getColor(bkg_news2));
         }
-
         newsTittle.setText(articles.getTitle());
         Log.d("NEWS", "bind: "+articles.getDescription());
         newsText.setText(articles.getDescription());
         Picasso.get().load(articles.getUrltoimage()).into(newsImage);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(articles.getUrl()));
+        itemView.getContext().startActivity(i);
     }
 }
